741b4a1a41a8b46d7f852f17274fe07780f24b1b
jsalamanca3 <jfsalamanca3@gmail.com>
2023-07-20 22:14:29 -0500

    preEntrega2SalamancaCh

--- src/components/BarraNavegacion/Logo.jsx
+++ src/components/BarraNavegacion/Logo.jsx
@@ -1,12 +1,12 @@
 import "./logo.css";
 import { Link } from "react-router-dom";
-
+import logo from '../../assets/logo192.png';
 
 function Logo() {
-    return (
-        <Link to="/" className="App-logo">
-          <img src="/"  alt="PóKemon"/>
-        </Link>
-    )
+  return (
+    <Link to="/" className="App-logo">
+      <img src={logo} alt="PóKemon" />
+    </Link>
+  )
 }
 export default Logo;
--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -3,7 +3,7 @@ import "./carrito.css";
 function CartWidget() {
     return (
         <div className="header-carrito">
-            <span class="material-symbols-outlined">
+            <span className="material-symbols-outlined">
             shopping_cart
             </span>
             <div className="header-cartWidget-carrito">
