f3a9555ec6ba07e99393bd8379344a5b185d8b00
Julián Fernando Salamanca Chacón <105863909+jsalamanca3@users.noreply.github.com>
2023-07-21 18:42:48 -0500

    PreEntrega2SalamancaCh

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -9,15 +9,14 @@ function ItemDetailContainer() {
   const [product, setProduct] = useState({});
   const { id } = useParams();
 
-
-  async function requestProduct() {
-    const respuesta = await getProductData(id);
-    setProduct(respuesta);
-  }
-
   useEffect(() => {
+    async function requestProduct() {
+      const respuesta = await getProductData(id);
+      setProduct(respuesta);
+    }
+
     requestProduct();
-  }, []);
+  }, [id]);
 
   return (
     <ItemDetail product={product} />
